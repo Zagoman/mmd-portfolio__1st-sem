7212c5b909167f764c0aea3649f4274789ce16cd
Zagoman <lucaszago.ph@gmail.com>
2022-01-08 20:51:15 +0100

    Add Burger Menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-const trigger = document.querySelector("#trigger");
+const navBtn = document.querySelector("#nav__btn");
+const navLinks = document.querySelector(".nav__links");
+const body = document.querySelector("body");
+
+navBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("is--hidden");
+  navBtn.classList.toggle("is--btn-x");
+  body.classList.toggle("is--overflow-hidden");
+});
